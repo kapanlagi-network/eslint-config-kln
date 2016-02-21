2332527ceee72d79272670094eb39fcf1c94abd3
Simon Degraeve <simon.degraeve@gmail.com>
2016-02-21 23:58:21 +0800

    style: fix linting issues

--- src/rules/stylistic-issues.js
+++ src/rules/stylistic-issues.js
@@ -86,7 +86,7 @@ export default {
     'space-in-parens': [2, 'never'],
     'space-infix-ops': 2,
     'space-unary-ops': [2, { words: true, nonwords: false }],
-    'spaced-comment': [2, 'always', { exceptions: ['-', '*']}],
+    'spaced-comment': [2, 'always', { exceptions: ['-', '*'] }],
     'wrap-regex': 2,
   },
 }
